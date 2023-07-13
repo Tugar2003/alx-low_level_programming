ac5f73266fc0c3444061b8e8a94be068ed610ce5
Tugar2003 <golanguage12@gmail.com>
2023-07-13 15:40:29 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-printf("lu\n", sizeof(char));
-printf("lu\n", sizeof(float));
-printf("lu\n", sizeof(int));
-printf("lu\n", sizeof(long long int));
-printf("lu\n", sizeof(unsigned char));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
+}
+
