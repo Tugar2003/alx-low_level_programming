28f7755c8d5c5ed16c9ef21b6b5470917152e4e7
Tugar2003 <golanguage12@gmail.com>
2023-07-24 10:03:14 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * reset_to_98 - updating a parameter value that points to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
