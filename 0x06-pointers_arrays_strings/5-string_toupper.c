dc64bad7a2aece972ed4a91129be34ac3878aec1
Tugar2003 <golanguage12@gmail.com>
2023-07-26 16:32:43 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * char *string_toupper - function that changes all lowercase
+ * letters of a string to uppercase.
+ * @str: string to be capitalized
+ *
+ * Return: A pointer to the changed string.
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		/*Convert the lowercase letter to uppercase*/
+			*ptr = *ptr - 'a' + 'A';
+	}
+	return (str);
+}
