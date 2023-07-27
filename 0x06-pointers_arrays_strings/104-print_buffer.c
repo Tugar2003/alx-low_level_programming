eb8241f65de94b5f250b07eb5183fac63be48cb2
Tugar2003 <golanguage12@gmail.com>
2023-07-27 02:58:32 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,50 +1,50 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
 
 /**
- * print_buffer -  function that prints a buffer
- * @b: pointer to a buffer
+ * print_buffer - prints buffer
+ * @b: buffer
  * @size: size
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int o, j, i;
+
+	o = 0;
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-
-	for (i = 0; i < size; i += 10)
+	while (o < size)
 	{
-		printf("%08x: ", i);
-
-		for (j = i; j < i + 10; j++)
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			if (j < size)
-				printf("%02x ", (unsigned char)b[j]);
+			if (i < j)
+				printf("%02x", *(b + o + i));
 			else
-				printf("   ");
-		}
-
-		printf(" ");
-
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
+				printf("  ");
+			if (i % 2)
 			{
-				char ch = b[j];
-
-				if (isprint(ch))
-					printf("%c", ch);
-				else
-					printf(".");
-			}
-			else
 				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
 		}
 		printf("\n");
+		o += 10;
 	}
 }
