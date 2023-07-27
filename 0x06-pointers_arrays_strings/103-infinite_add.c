987b0791576fe569e3edc0fa656dee786a280935
Tugar2003 <golanguage12@gmail.com>
2023-07-27 04:37:54 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,78 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * char *infinite_add - a function that adds two numbers
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
  * @size_r: buffer size
  * Return: pointer to calling function
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0;
-	int i, j;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-
-	if (size_r <= len1 || size_r <= len2)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	/*Start adding the digits from the least significant digits*/
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		int digit1 = (i >= 0) ? (n1[i] - '0') : 0;
-		int digit2 = (j >= 0) ? (n2[j] - '0') : 0;
-		int sum = digit1 + digit2 + carry;
-
-		if (size_r - 1 < 0)
-			/*Buffer overflow*/
-
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
 			return (0);
-		/*Store the least significant digit*/
-
-		r[size_r - 1] = sum % 10 + '0';
-		/*Calculate the carry for the next iteration*/
-		carry = sum / 10;
-
-	       size_r--;
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	/*Check if the result will fit within the buffer*/
-	if (size_r <= 0)
-		/*Buffer overflow*/
+	if (digits == size_r)
 		return (0);
-	/*Shift the result to the beginning of the buffer and null-terminate*/
-	memmove(r, r + size_r, strlen(r + size_r) + 1);
-
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
