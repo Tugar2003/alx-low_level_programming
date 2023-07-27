fcb61af671833cc8ec248901c5377b6c6a94ec85
Tugar2003 <golanguage12@gmail.com>
2023-07-27 05:07:15 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - a function that prints an integer.
+ * print_number - print numbers chars
  * @n: integer params
- * Return: 0.
+ * Return: 0
  */
+
 void print_number(int n)
 {
+	unsigned int n1;
+
+	n1 = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n1 = -n;
 	}
-	if (n / 10 != 0)
+
+	if (n1 / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(n1 / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar((n1 % 10) + '0');
 }
