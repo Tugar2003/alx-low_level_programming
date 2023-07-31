9ccea5f350b5c41442100f9d234bf079c6608fcf
Tugar2003 <golanguage12@gmail.com>
2023-07-31 10:13:45 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * char *_strchr - a function that locates a character in a string
+ * @s: a pointer to the input string
+ * @c: the seeking character in *s
+ *
+ * Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
