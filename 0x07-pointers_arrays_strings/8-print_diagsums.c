b80ec3fdf7e1c1f6d45bdc2debeafd0a380c9564
Tugar2003 <golanguage12@gmail.com>
2023-07-31 15:14:17 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+	int i;
+
+	/**
+	 * calculate the sum of the main diagonal
+	 * (top-left to bottom-right)
+	 */
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+	}
+
+	/**
+	 * calculate the sum of the second diagonal
+	 * (top-right to bottom-left)
+	 */
+	for (i = 0; i < size; i++)
+	{
+		sum2 += *(a + i * size + (size - 1 - i));
+	}
+	printf("Sum of main diagonal: %d\n", sum1);
+	printf("Sum of secondary diagonal: %d\n", sum2);
+}
