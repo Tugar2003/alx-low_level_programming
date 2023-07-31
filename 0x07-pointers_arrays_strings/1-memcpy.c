93e880743f165c953da2ca1fbb77e40f319d6f92
Tugar2003 <golanguage12@gmail.com>
2023-07-31 09:45:54 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * char *_memcpy - a function that copies memory area
+ * @dest: destination memory storage
+ * @src: source where memory is copied
+ * @n: number of bytes
+ *
+ * Return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i =0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
