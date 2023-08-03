03d64e74c4848f3471a4b92bd501e9e8e7a82bce
Tugar2003 <golanguage12@gmail.com>
2023-08-03 16:19:25 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("Program name: %s\n", argv[0]);
+	return (0);
+}
