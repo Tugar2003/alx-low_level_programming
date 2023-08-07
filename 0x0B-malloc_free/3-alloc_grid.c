e532884015f1637ecb0fc23cf9c89d5a53555c3a
Tugar2003 <golanguage12@gmail.com>
2023-08-07 14:11:52 -0700

    C - malloc, free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,49 +1,43 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
- * alloc_grid - function that returns a pointer to a 2 dim array
- * @width: input
- * @height: input
- * Return: pointers to 2 dim arrayy
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int h, i, w;
-	/*Check if width or height is 0 or negative,*/
+	int **mee;
+	int x, y;
+
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
-	/*Allocate memory for the 2D grid*/
-	grid = malloc(sizeof(int) * height);
-	if (grid == NULL)
-	{
+
+	mee = malloc(sizeof(int *) * height);
+
+	if (mee == NULL)
 		return (NULL);
-	}
-	/*Allocate memory for each row of the grid*/
-	for (h = 0; h < height; h++)
+
+	for (x = 0; x < height; x++)
 	{
-		grid[h] = malloc(sizeof(int) * width);
-		if (grid[h] == NULL)
+		mee[x] = malloc(sizeof(int) * width);
+
+		if (mee[x] == NULL)
 		{
-			/*If it fails free previous memory and return*/
-			for (i = 0; i < h; i++)
-			{
-				free(grid[i]);
+			for (; x >= 0; x--)
+				free(mee[x]);
+
+			free(mee);
+			return (NULL);
 		}
-		free(grid[i]);
-		return (NULL);
 	}
-}
-/*Initialize each element of the grid to 0*/
-for (h = 0; h < height; h++)
-{
-	for (w = 0; w < width; w++)
+
+	for (x = 0; x < height; x++)
 	{
-		grid[h][w] = 0;
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
 	}
-}
-return (grid);
+
+	return (mee);
 }
