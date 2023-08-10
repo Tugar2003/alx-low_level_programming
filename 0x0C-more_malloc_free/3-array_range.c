a9021b5775bf4201cf30cd3ae38b4e79271555fd
Tugar2003 <golanguage12@gmail.com>
2023-08-10 13:16:58 -0700

    C - More malloc, free

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * array_range - a function that creates an array of integers
+ * @min: minimum integer
+ * @max: maximum integer
+ * Return: NULL
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	arr = malloc((max - min + 1) * sizeof(int));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= max - min; i++)
+	{
+		arr[i] = min + i;
+	}
+
+	return (arr);
+}
