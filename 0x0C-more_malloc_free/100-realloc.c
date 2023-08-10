13957823a21da01f6e9b226b2ac9eaca23d3d924
Tugar2003 <golanguage12@gmail.com>
2023-08-10 13:43:31 -0700

    C - More malloc, free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _realloc - function that reallocates a memory block using malloc and free
+ * @ptr: pointer to prevous memory
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ * Return: pointer to the newly allocated memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int min_size;
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	min_size = (old_size < new_size) ? old_size : new_size;
+	memcpy(new_ptr, ptr, min_size);
+	free(ptr);
+
+	return (new_ptr);
+}
