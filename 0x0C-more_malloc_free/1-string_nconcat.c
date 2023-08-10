d12c32b07607b94e7ac64a923c17301f5f091660
Tugar2003 <golanguage12@gmail.com>
2023-08-10 10:47:51 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,38 +1,36 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * string_nconcat - function that concatenates two strings
  * @s1: string to append
- * @s2: string to concatenate from 
+ * @s2: string to concatenate from
  * @n: the number of bytes
  * Return: pointer to a newly allocated memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len_s1, len_s2;
-	char result;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	len_s1 = strlen(s1);
-	len_s2 = strlen(s2);
+	unsigned int len_s1 = strlen(s1);
+	unsigned int len_s2 = strlen(s2);
+	char *concat;
+	unsigned int len_concat;
 
 	if (n >= len_s2)
+	{
 		n = len_s2;
+	}
 
-	result = (char *)malloc(len_s1 + n + 1);
-	if (result == NULL)
+	len_concat = (len_s1 + n + 1);
+	concat = (char *)malloc(len_concat * sizeof(char));
+
+	if (concat == NULL)
 	{
 		return (NULL);
 	}
 
-	strncpy(result, len_s1);
-	strncpy(result, len_s2);
-	result [len_s1 + n] = '\0';
+	strcpy(concat, s1);
+	strncat(concat, s2, n);
+	concat[len_concat - 1] = '\0';
 
-	return (result);
+	return (concat);
 }
-
