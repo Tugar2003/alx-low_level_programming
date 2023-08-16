fca5adc9e9d9d8361f1637416d5705e6adc86a06
Tugar2003 <golanguage12@gmail.com>
2023-08-16 10:31:07 -0700

    C - Function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * main - entry point of the program
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: 0 on success, or specific exit status on error
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+	printf("Error\n");
+	exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (func == NULL)
+	{
+	printf("Error\n");
+	exit(99);
+	}
+
+	result = func(num1, num2);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
