ce2567de812125f623d4ed7b813170b468642d11
Tugar2003 <golanguage12@gmail.com>
2023-08-16 13:14:04 -0700

    C - Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,43 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
-
 /**
- * main - entry point of the program
- * @argc: number of command-line arguments
- * @argv: array of command-line arguments
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success, or specific exit status on error
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*func)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
-	printf("Error\n");
-	exit(98);
+		printf("Error\n");
+		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	func = get_op_func(argv[2]);
-
-	if (func == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
-	printf("Error\n");
-	exit(99);
+		printf("Error\n");
+		exit(99);
 	}
 
-	result = func(num1, num2);
-
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
-	printf("Error\n");
-	exit(100);
+		printf("Error\n");
+		exit(100);
 	}
 
-	printf("%d\n", result);
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
