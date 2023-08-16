e550fb29d585ecfc034a9e4aae3fb08f4324d7b8
Tugar2003 <golanguage12@gmail.com>
2023-08-16 13:18:30 -0700

    C - Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stdlib.h>
 
 /**
  * get_op_func - selects the correct function to perform the operation
@@ -18,11 +19,8 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op)
-	{
-	if (*(ops[i].op) == *s && *(s + 1) == '\0')
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
 	return (ops[i].f);
-	i++;
-	}
-	return (NULL);
 }
