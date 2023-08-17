b028f5586d4d3b334d11e186de716fd6f20b21e6
Tugar2003 <golanguage12@gmail.com>
2023-08-17 06:25:38 -0700

    C - Variadic functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - a function that prints strings
+ * @separator: pointer to constant character
+ * @n: number of string to be passed to the function
+ * @...: variable number of string arguements
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
