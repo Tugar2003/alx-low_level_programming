96871ebc71a4376b8dd369ab62d9d487ebf00280
Tugar2003 <golanguage12@gmail.com>
2023-08-24 13:41:07 -0700

    Singly linked lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+section .data
+	hello db "Hello, Holberton", 0
+	format db "%s"; 0
+
+section .text
+	extern printf
+	global main
+
+main:
+	push rdi	;
+	push rdi
+	push rax
+
+	lea rdi, [hello]  ;
+	lea rsi, [format]  ;
+	call printf	;
+
+
+	pop rax		;
+	pop rsi
+	pop rdi
+
+	ret		;
