13002b90a6233227d06f6345db4e1dcdab897573
Tugar2003 <golanguage12@gmail.com>
2023-08-24 17:39:41 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@ size_t print_list(const list_t *h)
 {
 	size_t cunt = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
