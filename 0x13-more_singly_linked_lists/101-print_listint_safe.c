5b00f066611a38b53b614c3e51c3246a458901dd
Tugar2003 <golanguage12@gmail.com>
2023-08-28 14:24:17 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,28 +8,31 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	  size_t count = 0;
-    const listint_t *current = head;
-    const listint_t *checker = head;
+	size_t count = 0;
+	const listint_t *current = head;
+	const listint_t *loop_node = NULL;
 
-    while (current != NULL)
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        count++;
-        current = current->next;
+	while (current != NULL)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
 
-        if (current == checker)
-        {
-            printf("-> [%p] %d\n", (void *)current, current->n);
-            exit(98);
-        }
+	/* Check if current node points to a previously visited node */
+	if (current->next >= current)
+	{
+		loop_node = current->next;
+		break;
+	}
 
-        if (checker != NULL)
-            checker = checker->next;
+		current = current->next;
+	}
 
-        if (checker != NULL)
-            checker = checker->next;
-    }
+	/* If a loop is detected, print " -> [loop_node_address] [loop_node_value]" */
+	if (loop_node != NULL)
+	{
+		printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
+		count++;
+	}
 
-    return count;
+	return (count);
 }
