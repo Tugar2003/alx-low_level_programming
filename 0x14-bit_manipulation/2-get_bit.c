1dea97516453178697f292d2717cc5270f911e02
Tugar2003 <golanguage12@gmail.com>
2023-08-31 09:06:58 -0700

    Bit manupulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: The number from which to get the bit
+ * @index: The index of the bit to get
+ *
+ * Return: specified index or -1 if an error occurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	return ((n >> index) & 1);
+}
