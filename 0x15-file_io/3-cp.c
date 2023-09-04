6a9e7bf085d2ac6beaabd58b4a57162880041a70
Tugar2003 <golanguage12@gmail.com>
2023-09-04 14:01:07 -0700

    C - File INPUT OR OUTPUT

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+void error_exit(int code, const char *format, ...);
+/**
+ * error_exit - Print an error message and exit with the given code
+ * @code: The exit code
+ * @format: The error message
+ * @...: arguement for the format string
+ */
+void error_exit(int code, const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	dprintf(STDERR_FILENO, format, args);
+	va_end(args);
+	exit(code);
+}
+
+/**
+ * main - Entry point
+ * @argv: Double pointer to character
+ * @argc: The number of arguement
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to\n");
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+
+		if (bytes_written != bytes_read)
+			error_exit(99,
+			"Error: Write did not write the expected amount of bytes to %s\n", argv[2]);
+	}
+	if (bytes_read == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	if (close(fd_from) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+	if (close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", fd_to);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,11 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdarg.h>
 
+#define BUFFER_SIZE 1024
+
+void error_exit(int code, const char *format, ...);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
